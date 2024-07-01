4a90ae089875fc1eecc2bb930b79cca36d8049b4
Lachlan Wilger <github.defender025@passmail.net>
2024-06-30 23:37:15 -0700

    Remap CapsLock to Escape and Control.

--- configuration.nix
+++ configuration.nix
@@ -158,6 +158,22 @@ in
 
   environment.etc."greetd/environments".text = "Hyprland";
 
+  services.interception-tools =
+    let
+      itools = pkgs.interception-tools;
+      itools-caps = pkgs.interception-tools-plugins.caps2esc;
+    in
+    {
+      enable = true;
+      plugins = [ itools-caps ];
+      udevmonConfig = pkgs.lib.mkDefault ''
+        - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 0 | ${itools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+    };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
