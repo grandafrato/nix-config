e8227c29b4db7861a80763845a7de83e15b925ad
Lachlan Wilger <github.defender025@passmail.net>
2024-08-10 00:45:56 -0700

    Enable bluetooth.

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,27 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
 
   virtualisation.containers.enable = true;
   virtualisation.podman = {
